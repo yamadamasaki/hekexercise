13052c7484cd96c163d2bb9cd8f6fedc4f288bf6
yamadamasaki <masaki@metabolics.co.jp>
2015-07-27 18:50:25 +0900

    gcontractsを使ってみた

--- grails-app/services/hekexercise/ReservationService.groovy
+++ grails-app/services/hekexercise/ReservationService.groovy
@@ -1,19 +1,24 @@
 package hekexercise
 
+import org.gcontracts.annotations.Requires
+
 //@Transactional
 class ReservationService {
     static transactional = true
 
+    @Requires({who && what && from && to && from < to})
     def reserve(Person who, Appliance what, Date from, Date to) {
         def r = new Reservation(who:who, what:what, fromDate:from, toDate:to)
         if (isBooked(r)) null
         else r.save()
     }
 
+    @Requires({what && when})
     def isReserved(Appliance what, Date when) {
         Reservation.findByWhatAndFromDateLessThanEqualsAndToDateGreaterThan(what, when, when)?true:false
     }
 
+    @Requires({reservation})
     def isBooked(reservation) {
         def query = Reservation.where {
             what == reservation.what && fromDate < reservation.toDate && toDate > reservation.fromDate
